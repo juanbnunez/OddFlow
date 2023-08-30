42e6a8984f24d98f0452ae8db0e3b2a982d91215
juanbnunez <contact@juanbnunez.info>
2023-08-30 14:41:48 -0600

    Changes

--- Server/song/song.go
+++ Server/song/song.go
@@ -1,5 +1,12 @@
 package song
 
+import (
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
+	"strings"
+)
+
 type Song struct {
 	ID       int
 	Name     string
@@ -7,10 +14,44 @@ type Song struct {
 	// Otros campos relevantes
 }
 
-// LoadSongsFromDirectory Funciones para cargar, agregar y eliminar canciones
+// LoadSongsFromDirectory loads songs from a directory and returns a slice of songs.
 func LoadSongsFromDirectory(directoryPath string) ([]Song, error) {
-	// Implementación para cargar canciones desde un directorio
-	return nil, nil
+	var songs []Song
+
+	files, err := ioutil.ReadDir(directoryPath)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, file := range files {
+		if file.IsDir() {
+			continue
+		}
+
+		if strings.HasSuffix(file.Name(), ".mp3") {
+			songPath := filepath.Join(directoryPath, file.Name())
+			song := Song{
+				Name:     strings.TrimSuffix(file.Name(), ".mp3"),
+				FilePath: songPath,
+			}
+			songs = append(songs, song)
+		}
+	}
+
+	return songs, nil
 }
 
-// Otras funciones relacionadas con la administración de canciones
+// Example usage
+func main() {
+	songs, err := LoadSongsFromDirectory("/path/to/songs/directory")
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	for _, song := range songs {
+		fmt.Printf("Song Name: %s\n", song.Name)
+		fmt.Printf("File Path: %s\n", song.FilePath)
+		fmt.Println("----")
+	}
+}
